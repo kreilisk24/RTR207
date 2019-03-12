99a36d60f395524475123a0699f756f1f9b01ab7
user <karlisk24@gmail.com>
2019-03-12 15:42:44 +0200

    2019_03_12

---
+++ praktiskais_2.m
@@ -0,0 +1,12 @@
+% syms x
+% f = x^2
+% solve(f, x)
+% diff()
+% int()
+% pretty() / collect()  - attelo izteiksmi labak
+% linspace() - skaitlu generators
+% vectorize() - funkcijai pieliek vajadzigos punktus
+% eval() - rekina funkciju
+% ezplot(x, y) - vienkarss grafiks
+% latex() - LaTeX funckija
+% vpa() - precizitates paligs
